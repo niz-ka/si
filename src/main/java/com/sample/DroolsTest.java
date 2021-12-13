221af4d4e840240c950dc1f3738290abab380d96
niz-ka <kamil.niznik2000@gmail.com>
2021-12-13 12:52:00 +0100

    Change global frame variable to local

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -3,9 +3,6 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import javax.swing.JDialog;
-import java.awt.*;
-import java.awt.event.*;
 
 public class DroolsTest {
 
@@ -15,19 +12,6 @@ public class DroolsTest {
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-        	
-        	// Create global frame
-        	JDialog frame = new JDialog();
-        	frame.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
-        	
-        	// Close application after frame close
-        	frame.addWindowListener(new WindowAdapter() { 
-        		@Override public void windowClosing(WindowEvent e) { 
-        			System.exit(0);
-        		}
-        	});
-        	
-        	kSession.setGlobal("frame", frame);
         	
             kSession.fireAllRules();
         } catch (Throwable t) {
